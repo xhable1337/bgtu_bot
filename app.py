8f0981341826df05d3ad65fa835c4a38efae519c
xhable1337 <36594349+xhable1337@users.noreply.github.com>
2019-04-22 13:07:23 +0300

    Update v2.1
    Added schedule of the school bells

--- app.py
+++ app.py
@@ -18,8 +18,12 @@ wday_wednesday = [no, 'Литература', 'Ин.язык', 'Физика', '
 wday_thursday = [no, 'Биология', 'Литература', 'Химия', 'ОБЖ', 'Математика', 'Астрономия', no, [no, 228, 318, 309, 303, 219, 217, no]]
 wday_friday = [no, 'Физика', 'Ин.язык', 'Информатика', 'Обществознание', 'Физкультура', 'Математика', no,[no, 217, '229/223', '202/204', 305, 'б/з', 219, no]]
 
+time_monday = ['8:00-8:40', '8:50-9:30', '9:50-10:30', '10:50-11:30', '11:40-12:20', '12:30-13:10', '13:20-14:00', '14:10-14:50']
+time_others = ['8:00-8:45', '8:55-9:40', '10:00-10:45', '11:05-11:50', '12:00-12:45', '12:55-13:40', '13:50-14:30', '14:40-15:20']
+
 bot = telebot.TeleBot(token)
 table = PrettyTable()
+table_r = PrettyTable()
 wd = datetime.datetime.today().isoweekday()
 
 
@@ -27,6 +31,12 @@ kbm = types.InlineKeyboardMarkup()
 kbm.row(types.InlineKeyboardButton(text='Расписание на сегодня', callback_data='today'))
 kbm.row(types.InlineKeyboardButton(text='Расписание на завтра', callback_data='tomorrow'))
 kbm.row(types.InlineKeyboardButton(text='Расписание по дням', callback_data='days'))
+kbm.row(types.InlineKeyboardButton(text='Расписание звонков', callback_data='rings'))
+
+kb_r = types.InlineKeyboardMarkup()
+kb_r.row(types.InlineKeyboardButton(text='Понедельник', callback_data='r_monday'))
+kb_r.row(types.InlineKeyboardButton(text='Остальные дни', callback_data='r_others'))
+kb_r.row(types.InlineKeyboardButton(text='В главное меню', callback_data='tomain'))
 
 kb_dn = types.InlineKeyboardMarkup()
 kb_dn.row(types.InlineKeyboardButton(text='Пн', callback_data='wday_monday'),
@@ -75,6 +85,28 @@ def button_func(call):
         bot.edit_message_text(chat_id=call.message.chat.id,
         message_id=call.message.message_id,
         text=text,
+        reply_markup=kbbb, parse_mode='Markdown')
+    elif call.data == 'rings':
+    	text = 'Выберите день недели:'
+    	bot.edit_message_text(chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=text,
+        reply_markup=kb_r, parse_mode='Markdown')
+    elif call.data == 'r_monday':
+    	table_r.clear()
+    	table_r.add_column(fieldname="№", column=index)
+    	table_r.add_column(fieldname="Время", column=time_monday)
+    	bot.edit_message_text(chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=f'Звонки на понедельник\n\n```{table_r}```',
+        reply_markup=kbbb, parse_mode='Markdown')
+     elif call.data == 'r_others':
+    	table_r.clear()
+    	table_r.add_column(fieldname="№", column=index)
+    	table_r.add_column(fieldname="Время", column=time_others)
+    	bot.edit_message_text(chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=f'Звонки на вт-пт\n\n```{table_r}```',
         reply_markup=kbbb, parse_mode='Markdown')
     elif call.data == 'tomorrow':
         table.clear()
