b80849efddb7b0df9132c2ba86899af67246a51d
xhable1337 <xhable.1995@gmail.com>
2020-09-06 17:04:21 +0300

    Update site_parser.py

--- site_parser.py
+++ site_parser.py
@@ -32,6 +32,7 @@ CHROMEDRIVER_PATH = 'chromedriver'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
 chrome_options.binary_location = CHROME_BIN
 
