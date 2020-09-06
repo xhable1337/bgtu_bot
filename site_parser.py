2b3736c5d9968588f52f62aff705ee5283bd40e3
xhable1337 <xhable.1995@gmail.com>
2020-09-06 19:59:12 +0300

    Update site_parser.py

--- site_parser.py
+++ site_parser.py
@@ -26,27 +26,26 @@ import re
 import datetime
 import os
 
-#CHROME_BIN = os.environ['GOOGLE_CHROME_SHIM']
-#CHROMEDRIVER_PATH = 'chromedriver'
-#
-#chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument('--headless')
-#chrome_options.add_argument('--disable-infobars')
-#chrome_options.add_argument('--disable-dev-shm-usage')
-#chrome_options.add_argument('--disable-browser-side-navigation')
-#chrome_options.add_argument('--disable-gpu')
-#chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
-#chrome_options.binary_location = CHROME_BIN
-firefox_options = webdriver.FirefoxOptions()
-firefox_options.headless = True
+CHROME_BIN = os.environ['GOOGLE_CHROME_SHIM']
+CHROMEDRIVER_PATH = 'chromedriver'
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-infobars')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-browser-side-navigation')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.206 (Edition Yx GX)")
+chrome_options.binary_location = CHROME_BIN
+#firefox_options = webdriver.FirefoxOptions()
+#firefox_options.headless = True
 
 
 
 MONGODB_URI = os.environ['MONGODB_URI']
 client = MongoClient(host=MONGODB_URI, retryWrites=False) 
 db = client.heroku_38n7vrr9
-#users = db.users
 schedule_db = db.schedule
 groups_db = db.groups
 users = db.users
@@ -84,8 +83,8 @@ def get_groups(faculty='Факультет информационных техн
     """
     if groups_db.find_one() is None:
         year = str(year)
-        #driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-        driver = webdriver.Firefox(options=firefox_options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+        #driver = webdriver.Firefox(options=firefox_options)
         #driver = webdriver.Chrome(executable_path='chromedriver.exe')
         url = 'https://www.tu-bryansk.ru/education/schedule/'
         driver.get(url)
@@ -111,8 +110,8 @@ def get_groups(faculty='Факультет информационных техн
     else:
         if force_update == True:
             year = str(year)
-            #driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-            driver = webdriver.Firefox(options=firefox_options)
+            driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+            #driver = webdriver.Firefox(options=firefox_options)
             #driver = webdriver.Chrome(executable_path='chromedriver.exe')
             url = 'https://www.tu-bryansk.ru/education/schedule/'
             driver.get(url)
@@ -185,8 +184,8 @@ def get_schedule(group, weekday, weeknum):
 
         }
 
-        #driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-        driver = webdriver.Firefox(options=firefox_options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+        #driver = webdriver.Firefox(options=firefox_options)
         #driver = webdriver.Chrome(executable_path='chromedriver.exe')
         url = 'https://www.tu-bryansk.ru/education/schedule/'
         driver.get(url)
