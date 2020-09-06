e0dd67b820d2b305d4538fc5980300192eb4d69d
xhable1337 <xhable.1995@gmail.com>
2020-09-06 17:26:12 +0300

    Update site_parser.py

--- site_parser.py
+++ site_parser.py
@@ -32,8 +32,10 @@ CHROMEDRIVER_PATH = 'chromedriver'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-infobars')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-browser-side-navigation')
 chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
 chrome_options.binary_location = CHROME_BIN
 
